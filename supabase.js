de146d48101e4e92a45997fbdfc93a5c1cd95e18
thigasHansen <skeith1st@gmail.com>
2025-12-09 20:26:12 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const SUPABASE_URL = "https://whrugfiojjbxkzjvtgjs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_3qJoVWoF-Kfn1n2dAi0RgA_gqT-j5uN";
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
