f4d5a536fa5685f9f8b249b8592103155cbde015
thigasHansen <skeith1st@gmail.com>
2025-12-10 18:38:59 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+const SUPABASE_URL = "https://whrugfiojjbxkzjvtgjs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_3qJoVWoF-Kfn1n2dAi0RgA_gqT-j5uN";
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
